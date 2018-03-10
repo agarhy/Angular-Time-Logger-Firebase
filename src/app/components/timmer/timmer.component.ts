753a6ea5809f58bd034aa779c24bb91a1963b607
Ahmed <garhy.ahmed@gmail.com>
2018-03-10 20:39:25 +0200

    fix timer reset on stop

--- src/app/components/timmer/timmer.component.ts
+++ src/app/components/timmer/timmer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { TimeLog,INITIAL_TIMELOG } from '../../models/time-log';
+import { TimeLog } from '../../models/time-log';
 import { LogsService } from '../../services/logs.service';
 import { DataService } from '../../services/data.service';
 
@@ -9,104 +9,120 @@ import * as moment from 'moment';
 
 
 @Component({
-  selector: 'app-timmer',
-  templateUrl: './timmer.component.html',
-  styleUrls: ['./timmer.component.css']
+	selector: 'app-timmer',
+	templateUrl: './timmer.component.html',
+	styleUrls: ['./timmer.component.css']
 })
 export class TimmerComponent implements OnInit {
 
-  timmer:TimeLog;
-  timerTitle;string;
-  startResume:boolean;
+	timmer:TimeLog;
+	timerTitle;string;
+	startResume:boolean;
 
-  constructor(
-    public _logsService:LogsService,
-    public _data:DataService
-  ) { 
+	constructor(
+		public _logsService:LogsService,
+		public _data:DataService
+		) { 
     //var duration = moment.duration(2000);   
     //console.log(moment.utc(duration.asMilliseconds()).format("HH:mm:ss.S"));
   }
 
   ngOnInit() {
-    
-    this.initTimerLog();
+  	
+  	this.initTimerLog();
 
      //Continue running timmer
-  	 if( JSON.parse(localStorage.getItem('timerOn')) ){
-  	 	
-  	 	this.timmer=JSON.parse(localStorage.getItem('timerLogRecord'));  	 
+     if( JSON.parse(localStorage.getItem('timerOn')) ){
+     	
+     	this.timmer=JSON.parse(localStorage.getItem('timerLogRecord'));  	 
 
-      if(this.timmer.running){
-        this.timmer.countMilliseconds= Date.now() - parseInt(localStorage.getItem('startOn'));
-    	 	this.timmerTick();
-      }
- 
-  	 }else{
+     	if(this.timmer.running){
+     		this.timmer.countMilliseconds= Date.now() - parseInt(localStorage.getItem('startOn'));
+     		this.timmerTick();
+     	}
+     	
+     }else{
 
      }
 
      //Resume log listner
      this._data.currentMsg.subscribe(msg=>{
-       console.log('Subject recived');
-       this.startResume=true;
-       this.resumeLog(msg);
+     	console.log('Subject recived');
+     	this.startResume=true;
+     	this.resumeLog(msg);
 
      })
-  }
+   }
 
-  initTimerLog(){
-    this.timmer = INITIAL_TIMELOG;
-    
-  }
+   initTimerLog(){
+   	this.timmer = {
+   		
+   		title:'',
+   		running:false,
+   		durations:<any>[],
+   		count:"00:00:00.0",
+   		countMilliseconds:0,  
+   		time:{
+   			mseconds:"0",
+   			seconds:"00",
+   			minutes:"00",
+   			hours:"00"
+   		},
+   		createdAt:null,
+   		updatedAt:null 
 
-  startTimmer(){
-  	
-  	console.log('start');
+   	};
+   	
+   }
+
+   startTimmer(){
+   	
+   	console.log('start');
    // console.log(this.timmer.$id);
- 
-  	this.timmer.running=true;
+   
+   this.timmer.running=true;
     //console.log(this.timmer)
     localStorage.setItem('timerOn','true');
-  	localStorage.setItem('startOn', Date.now().toString() );
-  	localStorage.setItem('timerLogRecord',JSON.stringify(this.timmer));
-  	this.startResume=false;
-	  this.timmerTick();
+    localStorage.setItem('startOn', Date.now().toString() );
+    localStorage.setItem('timerLogRecord',JSON.stringify(this.timmer));
+    this.startResume=false;
+    this.timmerTick();
   }
 
   timmerTick(){
-    var that=this
-    Observable.interval(100)
-    .takeWhile(() => this.timmer.running)
-    .subscribe(i => { 
-       that.timmer.countMilliseconds+=100;      
-       this.timmer.count=moment.utc(moment.duration(that.timmer.countMilliseconds).asMilliseconds()).format("HH:mm:ss.S");
-       this.timmer.time.mseconds=moment.utc(moment.duration(that.timmer.countMilliseconds).asMilliseconds()).format("S");
-       this.timmer.time.seconds=moment.utc(moment.duration(that.timmer.countMilliseconds).asMilliseconds()).format("ss");
-       this.timmer.time.minutes=moment.utc(moment.duration(that.timmer.countMilliseconds).asMilliseconds()).format("mm");
-       this.timmer.time.hours=moment.utc(moment.duration(that.timmer.countMilliseconds).asMilliseconds()).format("HH");
+  	var that=this
+  	Observable.interval(100)
+  	.takeWhile(() => this.timmer.running)
+  	.subscribe(i => { 
+  		that.timmer.countMilliseconds+=100;      
+  		this.timmer.count=moment.utc(moment.duration(that.timmer.countMilliseconds).asMilliseconds()).format("HH:mm:ss.S");
+  		this.timmer.time.mseconds=moment.utc(moment.duration(that.timmer.countMilliseconds).asMilliseconds()).format("S");
+  		this.timmer.time.seconds=moment.utc(moment.duration(that.timmer.countMilliseconds).asMilliseconds()).format("ss");
+  		this.timmer.time.minutes=moment.utc(moment.duration(that.timmer.countMilliseconds).asMilliseconds()).format("mm");
+  		this.timmer.time.hours=moment.utc(moment.duration(that.timmer.countMilliseconds).asMilliseconds()).format("HH");
 
-    })
-   
+  	})
+  	
   }
 
 
   stopTimmer(){
   	console.log('stop');
-  	 
+  	
   	this.timmer.running=false;  	
-    this.timmer.durations.push({
-      start:JSON.parse(localStorage.getItem('startOn')),
-      stop:Date.now()
-    })
+  	this.timmer.durations.push({
+  		start:JSON.parse(localStorage.getItem('startOn')),
+  		stop:Date.now()
+  	})
 
-    if(!this.timmer.$id){
+  	if(!this.timmer.$id){
       //add new record if no Id exists
       this.timmer.createdAt=parseInt(new Date().toString());
       this._logsService.addLog(this.timmer);
     }else{
-      console.log('log updated');
-      this.timmer.updatedAt=parseInt(new Date().toString());
-      this._logsService.updateLog(this.timmer);
+    	console.log('log updated');
+    	this.timmer.updatedAt=parseInt(new Date().toString());
+    	this._logsService.updateLog(this.timmer);
 
     }
     
@@ -124,30 +140,30 @@ export class TimmerComponent implements OnInit {
   }
 
   resumeLog(id:string){
-    console.log("resume started");
+  	console.log("resume started");
 
-    const _rec=this._logsService.getLogById(id);
+  	const _rec=this._logsService.getLogById(id);
 
-    _rec.subscribe(log=>{   
-      console.log('subject Obj:');      
-      console.log(log);      
-     if(this.startResume){
-       this.timmer=log;       
-       this.startTimmer();
-     }
-         
-    },
-     err => {
-    console.log('Received error:', err);
-    },
-    () => {
-        console.log('Completed');
-    }
-    );
+  	_rec.subscribe(log=>{   
+  		console.log('subject Obj:');      
+  		console.log(log);      
+  		if(this.startResume){
+  			this.timmer=log;       
+  			this.startTimmer();
+  		}
+  		
+  	},
+  	err => {
+  		console.log('Received error:', err);
+  	},
+  	() => {
+  		console.log('Completed');
+  	}
+  	);
 
-      
+  	
 
-    
+  	
   }
 
 }
--- src/app/models/time-log.ts
+++ src/app/models/time-log.ts
@@ -18,21 +18,3 @@ export interface TimeLog {
 	updatedAt:number
 	
 }
-
-export var INITIAL_TIMELOG:TimeLog = {
-	
-	title:'',
-	running:false,
-	durations:<any>[],
-	count:"00:00:00.0",
-	countMilliseconds:0,  
-	time:{
-		mseconds:"0",
-		seconds:"00",
-		minutes:"00",
-		hours:"00"
-	},
-	createdAt:null,
-	updatedAt:null 
-
-}
